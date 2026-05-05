c96cdf0e8b73d2aeb0d084d8ff5a3b0142539c3f
officialtavo <145372915+officialtavo@users.noreply.github.com>
2026-05-05 14:10:11 -0700

    Add files via upload

---
+++ scripts/objRefTable.js
@@ -0,0 +1,73 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.TiledBg,
+		C3.Plugins.Sprite,
+		C3.Behaviors.Platform,
+		C3.Behaviors.scrollto,
+		C3.Behaviors.solid,
+		C3.Behaviors.Sin,
+		C3.Behaviors.jumpthru,
+		C3.Plugins.Keyboard,
+		C3.Plugins.Text,
+		C3.Behaviors.Bullet,
+		C3.Behaviors.wrap,
+		C3.Plugins.Audio,
+		C3.Plugins.Keyboard.Cnds.OnKey,
+		C3.Behaviors.Platform.Acts.FallThrough,
+		C3.Plugins.Keyboard.Cnds.IsKeyDown,
+		C3.Behaviors.Platform.Acts.SimulateControl,
+		C3.Plugins.Sprite.Acts.SetMirrored,
+		C3.Plugins.Sprite.Cnds.CompareY,
+		C3.Plugins.System.Exps.layoutheight,
+		C3.Plugins.System.Acts.RestartLayout,
+		C3.Behaviors.Platform.Cnds.IsJumping,
+		C3.Plugins.Sprite.Acts.SetAnim,
+		C3.Plugins.Sprite.Cnds.OnCollision,
+		C3.Plugins.Sprite.Acts.Destroy,
+		C3.Plugins.System.Acts.AddVar,
+		C3.Plugins.System.Acts.SubVar,
+		C3.Plugins.System.Cnds.OnLayoutStart,
+		C3.Plugins.Audio.Acts.Play,
+		C3.Plugins.System.Acts.GoToLayout
+	];
+};
+self.C3_JsPropNameTable = [
+	{BackgroundTile: 0},
+	{Platform: 0},
+	{ScrollTo: 0},
+	{Player: 0},
+	{Solid: 0},
+	{SolidTile: 0},
+	{Sine: 0},
+	{MovingSolidTile: 0},
+	{Jumpthru: 0},
+	{JumpThruTile: 0},
+	{Keyboard: 0},
+	{Start: 0},
+	{Bullet: 0},
+	{Wrap: 0},
+	{enemy2: 0},
+	{lava: 0},
+	{key: 0},
+	{Sprite: 0},
+	{artworksegoMbcz6VBDQgOUbxvwcJwt500x: 0},
+	{Audio: 0},
+	{KeyCount: 0}
+];
+
+self.InstanceType = {
+	BackgroundTile: class extends self.ITiledBackgroundInstance {},
+	Player: class extends self.ISpriteInstance {},
+	SolidTile: class extends self.ITiledBackgroundInstance {},
+	MovingSolidTile: class extends self.ITiledBackgroundInstance {},
+	JumpThruTile: class extends self.ITiledBackgroundInstance {},
+	Keyboard: class extends self.IInstance {},
+	Start: class extends self.ITextInstance {},
+	enemy2: class extends self.ISpriteInstance {},
+	lava: class extends self.ITiledBackgroundInstance {},
+	key: class extends self.ISpriteInstance {},
+	Sprite: class extends self.ISpriteInstance {},
+	artworksegoMbcz6VBDQgOUbxvwcJwt500x: class extends self.ISpriteInstance {},
+	Audio: class extends self.IInstance {}
+}
